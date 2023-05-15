11f05ad1e515c4820786040e177c17a3f6b2332b
David Summers <davidfootball5@gmail.com>
2023-05-14 21:26:48 -0700

    Updates

---
+++ precache-manifest.3ac619603ffe91e50d1607f14b3f9550.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a72b2ec2b966f199052f75f6f996a9b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5673f6de617bb445ca53",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "08b5a20540cea4e34d7f",
+    "url": "/Portfolio/static/css/main.c778119a.chunk.css"
+  },
+  {
+    "revision": "5673f6de617bb445ca53",
+    "url": "/Portfolio/static/js/2.65cd997a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.65cd997a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b5a20540cea4e34d7f",
+    "url": "/Portfolio/static/js/main.2bbb3528.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
